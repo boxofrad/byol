899062dcf3c9f7b0ce640b0f2ce2d48a4d8d2471
Daniel Upton <daniel@floppy.co>
2016-01-27 11:21:10 +0000

    Add support for decimals

--- parsing.c
+++ parsing.c
@@ -17,7 +17,7 @@ int main(int argc, char **argv) {
 
   mpca_lang(MPCA_LANG_DEFAULT,
       "                                                            \
-        number     : /-?[0-9]+/ ;                                  \
+        number     : /-?[0-9]+(\\.[0-9]+)?/ ;                      \
         operator   : '+' | '-' | '*' | '/' ;                       \
         expression : <number> | '(' <operator> <expression>+ ')' ; \
         program    : /^/ <operator> <expression>+ /$/ ;            \
